191698cb17e1154584bd7fbc107ca080bd057dda
Wiktoria <50110543+wiktoriann@users.noreply.github.com>
2019-06-02 22:26:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-   function getdata()
-         {
-             
-          
+  function getdata()
+
+                {
           
             var field = $('#inp').val();
             
             if(field == '')
                 {
-                    alert('What are you looking for? Title field is empty!');
+                    alert('Czego szukasz? Nie podano żadnej frazy!');
                 }
             else
                 {
@@ -15,7 +14,7 @@
                     var covr = '';
                     var desc = '';
                     var ttl = '';
-                    
+                    var und = 'Ta książka nie ma jeszcze opisu :c';
                     
                     $.get('https://www.googleapis.com/books/v1/volumes?q= intitle:' + field, function(response)
                          {
@@ -25,17 +24,27 @@
                                    ttl = $('<div class="content"><div class="header">' + response.items[i].volumeInfo.title + '</div>');
                                    desc = $('<div class="desc">' + response.items[i].volumeInfo.description + '</div>');
                                    link = response.items[i].volumeInfo.imageLinks.thumbnail;
-                                   covr = $('<div class="cov"><img src="' + link + '"</div></div>');
-                                   
+                                   covr = $('<div class="cov"><img src="' + link + '" alt="Brak okładki"</div></div>');
+                                   var unddisc = $('<div class="desc">' + und + '</div>');
                     
-                                      
-                                   
+                                       
+                                   if(response.items[i].volumeInfo.description === undefined)
+                                       {
                                            
                                                 
                                                 ttl.appendTo('#serc_result');
+                                                unddisc.appendTo('#serc_result');
+                                                covr.appendTo('#serc_result');
+                                   
+                                       }
+                                   
+                                   else
+                                       {
+                                                ttl.appendTo('#serc_result');
                                                 desc.appendTo('#serc_result');
                                                 covr.appendTo('#serc_result');
-                                            
+                                       }
+                                  
                                    
                                         
                                    
@@ -44,6 +53,8 @@
                     
                     
                 }
-            
-            
-        }
+       
+ 
+    
+  }
+  
